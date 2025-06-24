2e5cfa0b7a8256bf0d79af63bbc9622e469356a3
Kradhityaw <kevindradhitya27@gmail.com>
2025-06-24 10:35:18 +0700

    Add API endpoints for Create, Update, Delete User

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -23,4 +23,42 @@ const getUser = async (req, res) => {
   }
 };
 
-module.exports = { getUsers, getUser };
+const createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body);
+
+    if (!user) {
+      return res.status(401).json({ message: "Pengguna tidak ditemukan" });
+    }
+
+    const updatedUser = await User.findById(req.params.id);
+    res.status(201).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(401).json({ message: "Pengguna tidak ditemukan" });
+    }
+
+    res.status(201).json({ message: "Behasil menghapus pengguna" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getUsers, getUser, createUser, updateUser, deleteUser };
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const { authenticate } = require("../middlewares/auth.middleware");
-const { getUsers, getUser } = require("../controllers/user.controller");
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/user.controller");
 
 const router = express.Router();
 
 router.get("/", authenticate, getUsers);
 router.get("/:id", authenticate, getUser);
+router.post("/", authenticate, createUser);
+router.put("/:id", authenticate, updateUser);
+router.delete("/:id", authenticate, deleteUser);
 
 module.exports = router;
